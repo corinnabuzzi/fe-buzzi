0054fe65223cf9a6012f31f3d4dbe7da0d091762
corinnabuzzi <corinnabuzzi48@gmail.com>
2026-04-21 12:26:37 +0200

    feat: esercizio 1.7

--- js/esercizi/esercizio1.js
+++ js/esercizi/esercizio1.js
@@ -1,22 +1,26 @@
 // 1.1: Crea un file esercizio1.js che stampi il tuo nome e cognome nella console.
+console.log("Esercizio 1.1")
 console.log("Corinna Buzzi")
 
 // 1.2 Scrivi un programma che stampi su righe separate: nome, età, città
+console.log("Esercizio 1.2")
 console.log("Corinna Buzzi")
 console.log("20")
 console.log("Milano")
 
 // 1.3 Scrivi un programma che stampi il risultato delle seguenti operazioni, ciascuna su una riga separata:
+console.log("Esercizio 1.3")
 console.log(15 + 27)
 console.log(100 - 58)
 console.log(7 * 8)
 console.log(144 / 12)
 
 // 1.4 Scrivi un programma che utilizzi un solo console.log() per stampare tre parole separate sulla stessa riga.
+console.log("Esercizio 1.4")
 console.log("Parola 1", "Parola 2", "Parola 3")
 
 // 1.5 Scrivi un programma che contenga almeno un commento su una riga e un commento su più righe. Il programma deve anche stampare un messaggio nella console.
-
+console.log("Esercizio 1.5")
 /*
 commento
 su
@@ -24,9 +28,16 @@ più
 righe
 */
 
-console.log("Esercizio 1.5: commento su una riga + commento su più righe")
-
 // 1.6: Scrivi un programma che stampi una breve presentazione di te stesso su più righe, usando più chiamate a console.log().
+console.log("Esercizio 1.7")
 console.log("Ciao! Mi chiamo Corinna Buzzi")
 console.log("Ho 20 anni")
 console.log("Sto studiando JavaScript")
+
+// 1.7 Scrivi un programma che stampi il tuo nome 5 volte, ciascuno su una riga diversa.
+console.log("Esercizio 1.7")
+console.log("Corinna Buzzi")
+console.log("Corinna Buzzi")
+console.log("Corinna Buzzi")
+console.log("Corinna Buzzi")
+console.log("Corinna Buzzi")
