2bcb490db641e095725197fa1dab76eecd1e30aa
corinnabuzzi <corinnabuzzi48@gmail.com>
2026-04-21 11:58:53 +0200

    feat: esercizio 1.2

--- js/esercizi/esercizio1.js
+++ js/esercizi/esercizio1.js
@@ -1,1 +1,7 @@
+// 1.1: Crea un file esercizio1.js che stampi il tuo nome e cognome nella console.
 console.log("Corinna Buzzi")
+
+// 1.2 Scrivi un programma che stampi su righe separate: nome, età, città
+console.log("Corinna Buzzi")
+console.log("20")
+console.log("Milano")
