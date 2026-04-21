186da63d07a5f4dd2900455deb7b1c0c3fe463c0
corinnabuzzi <corinnabuzzi48@gmail.com>
2026-04-21 12:39:37 +0200

    fix: edit estetico

--- js/esercizi/esercizio1.js
+++ js/esercizi/esercizio1.js
@@ -1,12 +1,14 @@
 // 1.1: Crea un file esercizio1.js che stampi il tuo nome e cognome nella console.
 console.log("Esercizio 1.1")
 console.log("Corinna Buzzi")
+console.log(" ")
 
 // 1.2 Scrivi un programma che stampi su righe separate: nome, età, città
 console.log("Esercizio 1.2")
 console.log("Corinna Buzzi")
 console.log("20")
 console.log("Milano")
+console.log(" ")
 
 // 1.3 Scrivi un programma che stampi il risultato delle seguenti operazioni, ciascuna su una riga separata:
 console.log("Esercizio 1.3")
@@ -14,10 +16,12 @@ console.log(15 + 27)
 console.log(100 - 58)
 console.log(7 * 8)
 console.log(144 / 12)
+console.log(" ")
 
 // 1.4 Scrivi un programma che utilizzi un solo console.log() per stampare tre parole separate sulla stessa riga.
 console.log("Esercizio 1.4")
 console.log("Parola 1", "Parola 2", "Parola 3")
+console.log(" ")
 
 // 1.5 Scrivi un programma che contenga almeno un commento su una riga e un commento su più righe. Il programma deve anche stampare un messaggio nella console.
 console.log("Esercizio 1.5")
@@ -27,12 +31,14 @@ su
 più
 righe
 */
+console.log(" ")
 
 // 1.6: Scrivi un programma che stampi una breve presentazione di te stesso su più righe, usando più chiamate a console.log().
 console.log("Esercizio 1.7")
 console.log("Ciao! Mi chiamo Corinna Buzzi")
 console.log("Ho 20 anni")
 console.log("Sto studiando JavaScript")
+console.log(" ")
 
 // 1.7 Scrivi un programma che stampi il tuo nome 5 volte, ciascuno su una riga diversa.
 console.log("Esercizio 1.7")
@@ -41,12 +47,14 @@ console.log("Corinna Buzzi")
 console.log("Corinna Buzzi")
 console.log("Corinna Buzzi")
 console.log("Corinna Buzzi")
+console.log(" ")
 
 // 1.8 Scrivi un programma che stampi una riga di 30 asterischi (*), poi il tuo nome centrato nella riga successiva, poi un'altra riga di 30 asterischi.
 console.log("Esercizio 1.8")
 console.log("******************************")
 console.log("           Corinna            ")
 console.log("******************************")
+console.log(" ")
 
 // 1.9 Scrivi un programma che stampi una "ricevuta" formattata come questa (i valori li decidi tu):
 console.log("Esercizio 1.9")
@@ -57,16 +65,16 @@ console.log("Prodotto: caffè")
 console.log("Quantità: 1")
 console.log("Prezzo: 1,20 euro")
 console.log("========================")
+console.log(" ")
 
 // 1.10 Scrivi un programma che stampi i risultati di queste espressioni: (10 + 5 * 2), ((10 + 5) * 2). Osserva la differenza e commenta nel codice perché i risultati sono diversi.
 console.log("Esercizio 1.10")
 console.log(10 + 5 * 2)
 console.log((10 + 5) * 2)
 // Il risultato è diverso perché sono operazioni diversi
-
+console.log(" ")
 
 // 1.11: Scrivi un programma che stampi il risultato di 0.1 + 0.2. Il risultato è quello che ti aspetti? Aggiungi un commento nel codice con la tua osservazione.
 console.log("Esercizio 1.11")
 console.log(0.1 + 0.2)
-
 // Il risultato è sbagliato.
