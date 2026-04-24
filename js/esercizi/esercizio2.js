e79bc97fa58a3f9f2f0713e4c66e7941812aa1f3
corinnabuzzi <corinnabuzzi48@gmail.com>
2026-04-24 10:07:07 +0200

    feat: es 2.2

--- js/esercizi/esercizio2.js
+++ js/esercizi/esercizio2.js
@@ -1,6 +1,16 @@
-// 2: Dichiara una variabile nome con il tuo nome e una costante annoNascita con il tuo anno di nascita. Stampa entrambi i valori.
+// 2.1: Dichiara una variabile nome con il tuo nome e una costante annoNascita con il tuo anno di nascita. Stampa entrambi i valori.
 
 let nome = "Corinna"
 const annoNascita = 2005
 
 console.log(nome, annoNascita)
+
+// 2.2: Dichiara due variabili numeriche a e b. Stampa il risultato della loro addizione, sottrazione, moltiplicazione, divisione e modulo.
+let a = 3
+let b = 2
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
