cd51d05402e3b97aa22df2f46be25a3c4d237aeb
corinnabuzzi <corinnabuzzi48@gmail.com>
2026-04-24 10:02:34 +0200

    feat: es 2.1

---
+++ js/esercizi/esercizio2.js
@@ -0,0 +1,6 @@
+// 2: Dichiara una variabile nome con il tuo nome e una costante annoNascita con il tuo anno di nascita. Stampa entrambi i valori.
+
+let nome = "Corinna"
+const annoNascita = 2005
+
+console.log(nome, annoNascita)
