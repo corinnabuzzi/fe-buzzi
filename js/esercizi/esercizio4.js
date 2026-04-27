8e56794d8d7f54941d36b2b035336958b960df4f
corinnabuzzi <corinnabuzzi48@gmail.com>
2026-04-27 10:47:28 +0200

    code: es 4.1

---
+++ js/esercizi/esercizio4.js
@@ -0,0 +1,5 @@
+// 4.1: Scrivi una arrow function benvenuto che accetti un nome come parametro e restituisca la stringa "Benvenuto, [nome]!".
+let benvenuto = nome => `Benvenuto, ${nome}!`
+console.log(benvenuto("Luca"))
+console.log()
+
